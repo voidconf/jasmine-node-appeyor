e3c0f0d3b1e1a6faf7fef0a29788c2a91cc0af2a
Chris Decoster <chris.decoster@3plearning.com>
2015-03-08 17:50:34 +1100

    test app veyor

---
+++ test/e2e/test-example.spec.js
@@ -0,0 +1,11 @@
+describe('integration test example', function(){
+
+	it('should run in appveyor', function(){
+		expect(1).toBe(2);
+	});
+
+	it('should run in appveyor and be valid', function(){
+		expect(1).toBe(1);
+	});
+	
+});
